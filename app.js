58f73db6a044b8d57d16d297685d7a8aebfe2ad1
luisxitq <luiselcrack183@gmail.com>
2026-09-24 00:17:59 -0500

    update app.js

--- app.js
+++ app.js
@@ -648,7 +648,7 @@ function generateKeyString() {
         for (let i = 0; i < 5; i++) s += chars.charAt(Math.floor(Math.random() * chars.length));
         return s;
     };
-    return `ITACHI-${segment()}-${segment()}-${segment()}`;
+    return `LIONX-${segment()}-${segment()}-${segment()}`;
 }
 
 // --- DATA LOADING & STATS ---
